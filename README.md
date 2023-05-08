97d3aa8674d81734d6604d6bec8d8a17504bae4e
Patrick Callery <37364932+baldpanda@users.noreply.github.com>
2023-05-08 19:49:32 +0100

    Add OpenVino model deployment for deploying inception resnet

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # openvino-model-server-k8s-terraform
 Repo for deploying Kubernetes cluster via Terraform as well as deploying and hosting a OpenVINO Model Server on it
+
+
+### Deploying OpenVino Model 
+
+helm upgrade -f open_vino_model_server/values.yaml ovms-app open_vino_model_server --set azure_storage_connection_string=$STORAGE_ACCOUNT_CONNECTION_STRING
---
+++ tests/acceptance/test_being_able_to_call_model.py
@@ -0,0 +1,33 @@
+from ovmsclient import make_grpc_client
+import numpy as np
+
+MODEL_URL = "20.82.217.204:80"
+MODEL_NAME = "inception-resnet"
+
+class TestCallingModel:
+    """Class for defining acceptance tests of being able to call model being deployed on infrastructure defined."""
+
+    def test_ok_response_returned_from_deployed_model(self):
+        """Should get OK response for valid request to model."""
+
+        # Arrange
+        sut = make_grpc_client(MODEL_URL)
+
+        with open("tests/acceptance/data/dog.jpeg", "rb") as f:
+            test_data = f.read()
+
+        payload = {"input": test_data}
+
+        with open("tests/acceptance/data/labels.txt") as f:
+            labels = f.readlines()
+        
+        expected_response = "Saint Bernard"
+
+
+        # Act
+        actual_response = sut.predict(inputs=payload, model_name=MODEL_NAME)
+        actual_predicted_image = labels[actual_response[0].argmax()].strip().replace("\n", "")
+
+
+        # Assert
+        assert expected_response == actual_predicted_image.strip().replace("\n", "")
